1d2af3620c7d01880840f9029d64fbb120c1bdbb
Fabio <fabiopetrosillo2@gmail.com>
2025-12-09 14:19:41 +0100

    Day2: part 2 completed

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
 using Utils;
 
 namespace Day2
@@ -12,26 +14,9 @@ namespace Day2
 
             List<string> lines = [.. util.ParseFile(util.GetFilePath())[0].Split(',')];
 
-            foreach (string line in lines)
-            {
-                var (start, limit) = logic.GetStartAndLimitId(line);
+            logic.FindInvalidIds(lines);
 
-                for(var i = start; i <= limit; i++)
-                {
-                    var stringOfI = i.ToString();
-                    if(stringOfI.Length %  2 != 0)
-                    {
-                        continue;
-                    }
-                    var halfedId = logic.GetStringHalfedString(stringOfI);
-                    if (halfedId.Item1 == halfedId.Item2) logic.ListOfIds.Add(i);
-
-                }
-            }
-
-            Console.WriteLine(logic.ListOfIds.Sum());
-
-
+            Console.WriteLine(logic.SetOfIds.Sum());
         }
     }
 }

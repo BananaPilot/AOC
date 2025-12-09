1e659c182b868c94d75238cc5fc455c433714c06
Fabio <fabioetrosillo2@gmail.com>
2025-12-09 23:17:31 +0100

    day3 part 1st part completed missing 2nd part

---
+++ Day3/Logic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Day3
+{
+    public class Logic
+    {
+        public List<long> ListOfJoltageRating = new List<long>();
+
+        private List<string> _strings = new List<string>();
+        public List<int> GetInsInLine(string input)
+        {
+            var returnList = new List<int>();
+            foreach(var num in input)
+            {
+                returnList.Add(int.Parse(num.ToString())); 
+            }
+
+            return returnList;
+        }
+
+        public Dictionary<int, int> GetGreatestNumberInList(List<int> ints)
+        {
+            //probably start from the last didigt to the first right to left
+            // for me of tomorrow you need to create a method that returns a map [indexOfGreatestNumber | GreatestNumber] then you need to sort the dictionary of inser the number in a list of string in a way that it keeps a natural line 
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Utils;
+
+namespace Day3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var util = new Util();
+            var logic = new Logic();
+
+            var lines = util.ParseFile(util.GetFilePath());
+
+            foreach ( var line in lines )
+            {
+                var ints = logic.GetInsInLine(line);
+                logic.GetRecursiveGreatestNumberInList(ints, 12);
+
+            }
+            Console.WriteLine(logic.ListOfJoltageRating.Sum());
+        }
+    }
+}
